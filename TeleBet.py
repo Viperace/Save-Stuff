4320dfd7912e5fc8509c37b0ac86a2b42d8996eb
Viperace <Viperace@users.noreply.github.com>
2018-07-31 18:18:20 +0800

    Update TeleBet.py

--- TeleBet.py
+++ TeleBet.py
@@ -1,129 +1,31 @@
-####################################
-# Gambling bot
-####################################
-/Start
-	-> Display Button Bet Size	[Wait user click]
-			[1]		[2]		[5]
-			[10]	[50]	[100]
-			[500]	[....	]
-	-> Display Button for 
-			[Up] 	[Down]
-	-> Display Button for 
-			Bet Up for 100b
-			[back] 	[Submit]		
-		[Wait user click]
-	-> send msg "/Bet up 100"
-	
-# Function to place bet 
-/Bet arg1 arg2
-	arg1, 	bet outcome 
-	arg2,	bet size
+# In main loop
+def main():
+	# Message Looper VH!
+	updater.job.queue.run_once(announcement_listener, 150 - 10) # start 10s earlier
 
-# Display current index
-/showindex
-
-# Show past historical outcome
-/pastresult 20
-	arg1, how many result to show
+# ************* VH ************
+# Use gevent to run? or schedulr ?
+gevent.spawn(announcement, 3, update)
 
 
-####################################
-# Result aggregator bot
-####################################
-[str,int] upUsers
-[str,int] downUsers
-float currentIndex
-float resultIndex
-roundInfo roundInfo
-
-@print_team
-List out upUsers and their bet
-List out downUsers and their bet
-
-@print_winners(up)
-List out only winners team
-
-@total_bet(outcome)
-Get total bet of up
-
-@roundInfo class
-	StartTime 	(in date and time)
-	EndTime		(in date and time)
-	startPoint
-	endPoint
-
-@Reconcile_Bet()
-#  get resultIndex,
-resultIndex = ShowIndex()
-
-if resultIndex > currentIndex
-	Outcome = UP
-else if resultIndex < currentIndex
-	Outcome = DOWN
-else #Draw ! 
-	Outcome = DRAW
-
-# IF DRAW, MOVE TO SPECIAL MOMENT
-
-
-#  get current win payout ratio
-if Outcome == UP
-	ratio = total_bet(Down)/total_bet(UP)
-else if Outcome == Down
-	ratio = total_bet(UP)/total_bet(Down)
-
-# get fee percentage
-if ratio > 100
-	fee = 0.15
-else if ratio > 50
-	fee = 0.10
-else if ratio > 10
-	fee = 0.08
-else if ratio > 2
-	fee = 0.05
-else if ratio > 0.9
-	fee = 0.05
-else if	ratio > 0.5
-	fee = 0.05
-else
-	fee = 0.025
-
-draw_fee = 0.05
-	
-# reallocate losers point to winners 
-if Outcome == UP
-	winners = upUsers
-	losers = downUsers
-else if Outcome == Down
-	winners = downUsers
-	losers = upUsers
-else if Outcome == DRAW
-	winners = upUsers
-	winners.Add(downUsers)
-else if Outcome == UN_RESOLVED 	# everyone lose
-	losers = upUsers
-	losers.Add(downUsers)
-
-########## UP/DOWN situation ##########	
-
-# Ask server to update points, send msg to all of them
-for(user in winners)
-	payout = betsize[user] * ratio * (1-fee)
-
-	PostServer(user, payout, roundInfo, outcome)
-	SendWinnerMsg(user, payout, roundInfo, outcome)
-
-for(user in losers)
-	PostServer(user, roundInfo, outcome)
-	SendLoserMsg(user, roundInfo, outcome)
-
-	
-@SendWinnerMsg(user, payout, roundInfo, outcome, )
-	.SendMSG(user, "Bet# " + roundInfo, " You bet " + betDir + " for $" + betsize[user])
-	.SendMSG(user, " You win" + payout + ".")
-
+# Will this block up all shit?
+def announcement_listener(update, interval):
+	announcement(update)
+	updater.job.queue.run_once(announcement_listener, interval)  # Call the next one
 		
-@SendLoserMsg(user, roundInfo, outcome, )
-	.SendMSG(user, "Bet# " + roundInfo, " You bet " + betDir + " for $" + betsize[user])
-	.SendMSG(user, " You lose")
+# How to pass update?		
+def announcement(update):
+	refresh_interval = 1
+	while True:
+		first_check_game_ended = redisdb.ttl('gamesEnd')
+		try:
+			time.sleep(refresh_interval) 
+			second_check_game_ended = redisdb.ttl('gamesEnd')
+			if first_check_game_ended != second_check_game_ended:
+				# DO ANNOUNCEMENT HERE
+				msg = "REsult is ..."
+				update.message.reply_text(msg)
 
+		except Exception,e:
+			print e
+		
