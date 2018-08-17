0250564e4571e4bad999f1f3eb418ee471eb124e
Viperace <Viperace@users.noreply.github.com>
2018-08-17 17:37:58 +0800

    Update TeleBet.py

--- TeleBet.py
+++ TeleBet.py
@@ -1,37 +1,90 @@
-# In main loop
-def main():
-	# Message Looper VH!
-	updater.job.queue.run_once(announcement_listener, 150 - 10) # start 10s earlier
-
-# ************* VH ************
-# Use gevent to run? or schedulr ?
-gevent.spawn(announcement, 3, update)
+from netunit import myGet
+import json
 
 
-# Will this block up all shit?
-def announcement_listener(update, interval):
-	announcement(update)
-	updater.job.queue.run_once(announcement_listener, interval)  # Call the next one
+def is_payment_sent(telid):
+	""" Check if user completed payment """
+	try:
+		domain = myGet()	
+		url = domain + "/pay/scanajax/?telid=" + telid
+		jsonurl = urllib.urlopen(url)
+		pay_status_json = json.loads(jsonurl.read())
+		status = pay_status_json['btcovertime']
+		if status == 'over':
+			return True
+		elif status == 'run':
+			return False
+		else:
+			print 'Unknown payment status ' + status
+			return False		
+	except:
+		print 'Unknown error when checking is_payment_sent '
+		return False
+	
+def get_payment_address(telid):
+	"""Function to request wallet address and get time left"""	
+	url = 'http://155.254.49.141:8111/pay/buybtc/?data=buy&telid=' + telid
+	jsonurl = urllib.urlopen(url)
+	j = json.loads(jsonurl.read())
+	btc_address = j['getbtcaddress']
+	time_left_in_sec = j['btcovertime']
+	#usd_per_btc = j['usd'] ??
+	#cny = j['cny'] ??
+	
+	return btc_address, time_left_in_sec
+	
+
+def topup_process(bot, update, offset_sec=60):
+	'''Initiate topup process for the user. Will Print the btc address to the user'''
+	telid = update.message.chat_id
+	
+	general_msg = "Notice: x-coin is automatically credited to your balance once we receive 6 confirmation from your Bitcoin. \
+		You can send any amount of Bitcoin, they are exchanged at this rate \
+		\n	0.0001 BTC = 100 x-coin "
+
+	# Key to save to redisb
+	user_btcaddress_key = telid + "__btcaddress" 
+	
+	# Check if got unexpired pending payment		
+	if is_payment_sent(telid):
+		# Get fresh address for user to send 
+		new_address, time_left = get_payment_address(telid)
+		time_left_sec = int(time_left)
 		
-# How to pass update?		
-def announcement(update):
-	refresh_interval = 1
-	while True:
-		first_check_game_ended = redisdb.ttl('gamesEnd')
-		try:
-			time.sleep(refresh_interval) 
-			second_check_game_ended = redisdb.ttl('gamesEnd')
-			if first_check_game_ended != second_check_game_ended:
-				# DO ANNOUNCEMENT HERE
-				msg = "REsult is ..."
-				update.message.reply_text(msg)
-
-		except Exception,e:
-			print e
+		# Save the address to redisb memory. Future pulling
+		expire_time = time_left_sec - offset_sec	 # Second before expired.
+		redisdb.setex(user_btcaddress_key, new_address, expire_time)
 		
-
-# Reward allocator, faster version:
-for i in range(0, len(self.winners_df)):
-	winner_id = self.winners_df.iloc[i]['user_id']
-	winner_newcoin = self.winners_df.iloc[i]['coin_to_add']
-	Extend_user.objects.filter(ext_user_id=winner_id).update(xcoin=winner_newcoin)
+		# Reply user
+		update.message.reply_text(general_msg)
+		update.message.reply_text('Please send bitcoin to this address \n' + new_address)
+		update.message.reply_text('Address will expire in ' + time_left + ' sec')		
+	else:
+		# Load the  btc address and time left
+		user_btcaddress = redisdb.get(user_btcaddress_key)			
+		time_left = redisdb.ttl(user_btcaddress_key)
+		
+		if time_left > 0:
+			# Still got time, reply user the same address 
+			update.message.reply_text(general_msg)
+			update.message.reply_text('Pending your bitcoin payment to this address \n' + user_btcaddress)
+			update.message.reply_text('Address will expire in ' + time_left + ' sec')
+		else:
+			# Lock period, wait for 'get_payment_address' to gen 'over' status
+			update.message.reply_text("Address expired. \nPlease do NOT send to the address and request for new \
+			address after one minute")
+		
+		
+from btchtm.models import pay_payhistory
+def show_payment_history(bot, update):
+	"""Function to pull sql database for user's history """
+	# pay_payhistory.objects.filter(userid=update.message.chat_id)
+	
+	# Retrieve all history 
+	
+	# Filter for the last 10
+	history_to_show
+	
+	# update.message.reply_text
+	update.message.reply_text(history_to_show)
+	
