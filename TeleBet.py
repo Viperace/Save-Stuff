aa5856e00a37d4fbfe8576527910725403c9b59f
Viperace <Viperace@users.noreply.github.com>
2018-09-12 14:40:27 +0800

    Update TeleBet.py

--- TeleBet.py
+++ TeleBet.py
@@ -1,90 +1,100 @@
-from netunit import myGet
-import json
+def start():
+	# User start 
+	# Get telid, from args[0] get upid
+	get_or_set_upid(telid, upid)
+	
+	
+# Method 2. Call from a button.
+def claim_reward():
+	'''
+	Process to give all unclaimed reward to user, send a message telling him that. 
+	Search through SQL up_id for all unclaimed reward, send one lumpsum to the user, and reset the indicators
+	'''
+	# Check all pending reward belong to this user
+	userid = get_userid_from_telid(user_telid)
+	user_downline_data = up_id.objects.filter(upid=userid)
+	
+	# Loop each of them and provide reward
+	total_reward = 0
+	total_new_friends = 0
+	for down_user in user_downline_data:
+		if down_user.upid_get_reward == True:
+			# Record reward
+			total_reward += 300
+			total_new_friends += 1
+			
+			# Reset indicator
+			down_user.upid_get_reward = False
+			down_user.save()
 
-
-def is_payment_sent(telid):
-	""" Check if user completed payment """
-	try:
-		domain = myGet()	
-		url = domain + "/pay/scanajax/?telid=" + telid
-		jsonurl = urllib.urlopen(url)
-		pay_status_json = json.loads(jsonurl.read())
-		status = pay_status_json['btcovertime']
-		if status == 'over':
-			return True
-		elif status == 'run':
-			return False
-		else:
-			print 'Unknown payment status ' + status
-			return False		
-	except:
-		print 'Unknown error when checking is_payment_sent '
-		return False
-	
-def get_payment_address(telid):
-	"""Function to request wallet address and get time left"""	
-	url = 'http://155.254.49.141:8111/pay/buybtc/?data=buy&telid=' + telid
-	jsonurl = urllib.urlopen(url)
-	j = json.loads(jsonurl.read())
-	btc_address = j['getbtcaddress']
-	time_left_in_sec = j['btcovertime']
-	#usd_per_btc = j['usd'] ??
-	#cny = j['cny'] ??
-	
-	return btc_address, time_left_in_sec
-	
-
-def topup_process(bot, update, offset_sec=60):
-	'''Initiate topup process for the user. Will Print the btc address to the user'''
-	telid = update.message.chat_id
-	
-	general_msg = "Notice: x-coin is automatically credited to your balance once we receive 6 confirmation from your Bitcoin. \
-		You can send any amount of Bitcoin, they are exchanged at this rate \
-		\n	0.0001 BTC = 100 x-coin "
-
-	# Key to save to redisb
-	user_btcaddress_key = telid + "__btcaddress" 
-	
-	# Check if got unexpired pending payment		
-	if is_payment_sent(telid):
-		# Get fresh address for user to send 
-		new_address, time_left = get_payment_address(telid)
-		time_left_sec = int(time_left)
+	# Give reward to user
+	if total_reward > 0:
+		# Give reward
+		userdata = Extend_user.objects.get(telid=telid)
+		userdata.xcoin += total_reward
+		userdata.save()
 		
-		# Save the address to redisb memory. Future pulling
-		expire_time = time_left_sec - offset_sec	 # Second before expired.
-		redisdb.setex(user_btcaddress_key, new_address, expire_time)
-		
-		# Reply user
-		update.message.reply_text(general_msg)
-		update.message.reply_text('Please send bitcoin to this address \n' + new_address)
-		update.message.reply_text('Address will expire in ' + time_left + ' sec')		
+		# Send msg to this user saying 'u gained xxx for inviting XX friends' !!
+		bot.send_message(chat_id=update.message.chat_id,
+                         text=str(total_new_friends) + " of your friends have joined! You are rewarded " 
+						 + str(total_reward) + " x-coin.")
 	else:
-		# Load the  btc address and time left
-		user_btcaddress = redisdb.get(user_btcaddress_key)			
-		time_left = redisdb.ttl(user_btcaddress_key)
+		bot.send_message(chat_id=update.message.chat_id,
+                         text="No new friend has joined yet. Invite more friends to gained free x-coin.")
+
+
+def get_or_set_upid(user_telid, upid):
+	''' 
+	Function to save upid of a particular userid into SQL.
+	Provided if the user do not already have existing upid.
+	'''
+	# Get corresponding userid
+	userid = get_userid_from_telid(user_telid)
+	
+	# Check if SQL already contain it 
+	if up_id.objects.filter(userid=userid).count() == 0:
+		try:
+			#up_id.objects.get_or_create(userid=userid, upid=upid)
+			up_id.objects.get_or_create(userid=userid, upid=upid, upid_get_reward=True)
+			return True
+		except:
+			print "Fail to create upid for user " + str(user_telid)
+			return False
+	else:
+		# Already contain up_id.
+		print "User " + str(user_telid) + " already contain up_id "
+		return False
+
+def get_userid_from_telid(telid):
+	''' Function to convert userid to telid '''
+	try:
+		userdata = Extend_user.objects.get(telid=telid)
+		return userdata.ext_user_id
+	except:
+		print "Error finding telid of user " + str(telid)
+		return None
+
 		
-		if time_left > 0:
-			# Still got time, reply user the same address 
-			update.message.reply_text(general_msg)
-			update.message.reply_text('Pending your bitcoin payment to this address \n' + user_btcaddress)
-			update.message.reply_text('Address will expire in ' + time_left + ' sec')
-		else:
-			# Lock period, wait for 'get_payment_address' to gen 'over' status
-			update.message.reply_text("Address expired. \nPlease do NOT send to the address and request for new \
-			address after one minute")
+
+# Method X - NO USE 
+def downline_user_start():
+	# Check if he is first time login. If up_id in SQL does not contains it, then yes
+	get_or_set_upid(telid)
+	
+	# Give reward to the user's upid, if not yet 
+	userid = get_userid_from_telid(user_telid)
+	user_upid_data = up_id.objects.get(userid=userid)
+	if user_upid_data.upid_get_reward == True:
+		# Up_id is eligible for reward
+		up_user = Extend_user.objects.get(ext_user_id=user_upid_data.upid)
 		
+		# Add reward to him
+		up_user.xcoin += 300
+		up_user.save()
 		
-from btchtm.models import pay_payhistory
-def show_payment_history(bot, update):
-	"""Function to pull sql database for user's history """
-	# pay_payhistory.objects.filter(userid=update.message.chat_id)
-	
-	# Retrieve all history 
-	
-	# Filter for the last 10
-	history_to_show
-	
-	# update.message.reply_text
-	update.message.reply_text(history_to_show)
-	
+		# Set the indicator back
+		user_upid_data.upid_get_reward = False
+		user_upid_data.save()
+	else:
+		# Reward already given
+		pass
