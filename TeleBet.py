965a404d186cd62cf69114bafb5bb3a6df59e595
Viperace <Viperace@users.noreply.github.com>
2018-09-17 14:16:32 +0800

    Update TeleBet.py

--- TeleBet.py
+++ TeleBet.py
@@ -7,36 +7,62 @@ upid_get_reward = 0, 1, 2
 1: Check for '0' status,
 	If downline user payhistory, sum of total value is > 1000, then set to '1'
 	Allow claim of 50
+
 2: Check for '1' status,
 	If user has claimed, then change to '2'
 
 def claim_reward(bot, update, telid):
     """
     Process to give all unclaimed reward to user, send a message telling him that.
-    Search through SQL up_id for all unclaimed reward, send one lumpsum to the user, and reset the indicators
+    Search through SQL up_id for all unclaimed reward, send one lump-sum to the user, and reset the indicators
 
-    User must topup at least 100 x-coin, in order for upid user to gain reward 5 xcoin
+    User must top-up at least 2000 x-coin, in order for upid user to gain reward 100 xcoin
+	
+	upid_get_reward = 0, 1, 2
+		0: Initialized, Unclaimable
+		1: Claimable
+		2: Already claimed
     """
+	from pay.models import Payhistory
+	
+	topup_threshold = 2000  # Amount a downline user need to topup for up user to be eligible for reward
+	reward_size = 100		# Reward amount for each downline user success
     query = update.callback_query
 
     # Check all pending reward belong to this user
     userid = get_userid_from_telid(telid)
     user_downline_data = up_id.objects.filter(upid=userid)
 
-    # Loop each of this user's downline, and save
-    total_reward = 0
+    # Loop each of this user's downline, and check if they have satisfied top-up condition
+	# 
     total_new_friends = 0
+	total_reward = 0
     for down_user in user_downline_data:
-        if down_user.upid_get_reward == True:
-            # Record reward
-            total_reward += 300
+		# To check if top-up condition reached, update indicator if it is
+        if down_user.upid_get_reward == 0:  
+            # Check this user_id's topup history
+			topup_history = Payhistory.objects.filter(userid=down_user.userid)
+			if len(topup_history) > 0:
+				# Get total x-coin topup
+				topup_amount = topup_history.aggregate(Sum('value'))/100	
+				
+				# Condition reached, set indicator
+				if topup_amount > topup_threshold:			
+					down_user.upid_get_reward = 1
+					down_user.save()
+		
+		# To check if redeemable, redeem if it is
+		if down_user.upid_get_reward == 1:
+			# Update total reward to give
             total_new_friends += 1
-
-            # Reset indicator
-            down_user.upid_get_reward = False
+			total_reward += reward_size
+			
+			# Save sql
+			down_user.upid_get_reward = 2
             down_user.save()
+            
 
-    # Give reward to user
+    # Give rewards to user
     if total_reward > 0:
         # Give reward
         userdata = Extend_user.objects.get(telid=telid)
@@ -45,8 +71,8 @@ def claim_reward(bot, update, telid):
 
         # Send msg to this user saying 'u gained xxx for inviting XX friends' !!
         bot.send_message(chat_id=query.message.chat_id,
-                         text=str(total_new_friends) + " of your friends have joined! You are rewarded "
+                         text=str(total_new_friends) + " of your friends have recently top-up! You are rewarded "
                               + str(total_reward) + " x-coin.")
     else:
         bot.send_message(chat_id=query.message.chat_id,
-                         text="No new friend has joined yet. Invite more friends to gained free x-coin.")
+                         text="No new friend has top-up yet. Invite more friends for more chance to win free x-coin.")
